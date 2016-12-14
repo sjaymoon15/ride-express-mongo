f75419f7da68370d55f25275c9d367f5dc802713
Seungjae Moon <sjaymoon15@gmail.com>
2016-12-14 13:49:31 -0500

    test driver controller

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const routes = require('./routes/routes');
+const mongoose = require('mongoose');
 const app = express();
+const bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/ride');
+
+app.use(bodyParser.json());
 
 routes(app);
 
